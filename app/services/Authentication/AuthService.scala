45478dc165a72081c4930bf12986a0a0dc0b4e9b
David Kunzmann <david.kunzmann@epfl.ch>
2018-06-20 10:51:56 +0200

    Added OIDC endpoints

---
+++ app/services/Authentication/AuthService.scala
@@ -0,0 +1,11 @@
+package services.Authentication
+
+import play.api.mvc.Headers
+
+import scala.concurrent.Future
+
+trait AuthService {
+  type U
+
+  def getUserInfo(headers: Headers): Future[U]
+}
